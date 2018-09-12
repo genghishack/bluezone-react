30f54e08c0b4ab406d50ba58c1bdd907680e124e
Chris Wade <genghishack@gmail.com>
2018-09-11 21:12:32 -0600

    Set zoom and center values for map, added controls

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, {
+  Layer,
+  Feature,
+  ZoomControl,
+  ScaleControl,
+  RotationControl,
+  Popup,
+} from "react-mapbox-gl";
 
 const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiZ2VuZ2hpc2hhY2siLCJhIjoiZ2x6WjZhbyJ9.P8at90QQiy0C8W_mc21w6Q"
@@ -15,7 +22,14 @@ export class MyMap extends Component {
           height: "100vh",
           width: "100vw"
         }}
+        center={[-97.000000, 38.000000]}
+        zoom={[3.7]}
       >
+        <ZoomControl />
+        <ScaleControl
+          measurement="mi"
+        />
+        <RotationControl />
       </Map>
     );
   }
