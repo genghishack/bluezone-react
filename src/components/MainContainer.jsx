f60acbcb56924e07d1850c2b563bedf3f9698d8a
Chris Wade <genghishack@gmail.com>
2018-09-14 14:10:47 -0600

    functions to focus and filter map view on state and district

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -38,16 +38,60 @@ class MainContainer extends Component {
     }
 
     // console.log(feature);
+    // console.log(map.getStyle());
+    // console.log(map.getFilter('districts_1'));
 
-    const view = geoViewport.viewport(bboxes[feature.properties.state + feature.properties.number], [window.innerWidth/3, window.innerHeight/3])
-    map.easeTo(view);
+    this.focusMap(map, feature.properties.state, feature.properties.number);
 
     this.setState({
       feature: feature,
     });
   };
 
+  filterMap = (map, stateAbbr, districtCode) => {
+    for (var i = 1; i <= 5; i++) {
+      let existingFilter = map.getFilter('districts_' + i);
+      if (existingFilter[0] === 'all') {
+        existingFilter = existingFilter[existingFilter.length - 1];
+      }
+      const filter = ['all'];
+      if (stateAbbr) filter.push(['==', 'state', stateAbbr]);
+      if (districtCode) filter.push(['==', 'number', districtCode]);
+
+      const layerFilter = filter.concat([existingFilter]);
+      map.setFilter('districts_' + i, layerFilter);
+      map.setFilter('districts_' + i + '_boundary', layerFilter);
+      map.setFilter('districts_' + i + '_label', layerFilter);
+    }
+  };
+
+  focusMap = (map, stateAbbr, districtCode) => {
+    const view = geoViewport.viewport(
+      bboxes[stateAbbr + districtCode],
+      [window.innerWidth/2.75, window.innerHeight/2.75]
+    );
+    map.easeTo(view);
+  };
+
   render() {
+
+    console.log(window.location.hash);
+
+    if (window.location.hash) {
+      const hash = window.location.hash;
+      const hashData = hash.substring(1).split('&').map(d => d.split('='));
+      let state;
+      let district;
+      hashData.map(d => {
+        if (d[0] === 'state') state = d[1];
+        if (d[0] === 'district') district = d[1];
+      });
+
+      if (state || (state && district)) {
+        console.log(state, district);
+      }
+
+    }
 
     return (
       <div id="main-container">
